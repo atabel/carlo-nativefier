d553c6369104fa070933c2b3fd3a83a2d61b6e99
Abel Toledano <atabel87@gmail.com>
2018-12-07 14:53:15 +0100

    fix bug with file paths

--- cli.js
+++ cli.js
@@ -25,6 +25,7 @@ if (!name) {
 
 (async () => {
   let iconFileName = null;
+  let iconFilePath = null;
 
   try {
     console.log("Searching for icon...");
@@ -32,7 +33,8 @@ if (!name) {
     console.log("Icon found: ", icon.source);
 
     iconFileName = "icon" + icon.ext;
-    fs.writeFileSync(iconFileName, icon.data);
+    iconFilePath = path.join(__dirname, iconFileName);
+    fs.writeFileSync(iconFilePath, icon.data);
   } catch (e) {
     console.log("Icon not found");
   }
@@ -43,15 +45,17 @@ if (!name) {
     .replace(/__NAME__/g, JSON.stringify(name))
     .replace(/__ICON__/g, JSON.stringify(iconFileName));
 
-  const appFileName = "app.js";
+  const appFilePath = path.join(__dirname, "app.js");
 
-  fs.writeFileSync(appFileName, file);
+  fs.writeFileSync(appFilePath, file);
 
   console.log("building...");
 
-  pkg.exec([appFileName, "--target", "host", "--output", name]).then(() => {
-    fs.unlinkSync(iconFileName);
-    fs.unlinkSync(appFileName);
+  pkg.exec([appFilePath, "--target", "host", "--output", name]).then(() => {
+    fs.unlinkSync(appFilePath);
+    if (iconFilePath) {
+      fs.unlinkSync(iconFilePath);
+    }
     console.log("done!");
   });
 })();
